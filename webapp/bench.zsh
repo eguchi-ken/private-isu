4ac8ae2533834fdbe5fcc33d59eb4ce7077ec779
Eguchi Ken <eguchi.ken@moneyforward.co.jp>
2023-11-19 16:41:49 +0900

    🔧 config: Add build script

---
+++ webapp/bench.zsh
@@ -0,0 +1,17 @@
+DIR=`git rev-parse --show-toplevel`
+rm $DIR/webapp/log/*
+# touch $DIR/webapp/log/{access,error,mysql-slow}.log
+
+# 再起動する
+docker compose build app
+docker compose down
+docker compose up -d
+
+sleep 10
+
+echo "START BENCHMARK"
+
+docker run --rm --network host -i private-isu-benchmarker /opt/go/bin/benchmarker -t http://host.docker.internal -u /opt/go/userdata
+
+./pt-query-digest log/mysql-slow.log > ./log/query-digest.sql
+alp json --reverse --sort sum --file ./log/access.log > ./log/access-digest.log
